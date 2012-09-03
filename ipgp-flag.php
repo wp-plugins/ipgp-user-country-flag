ee036748197ce533d67035e00c7983de1665d514
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-03 08:26:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ipgp-user-country-flag/tags/0.3@593901 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ipgp-flag.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Ipgp User Country Flag
+Plugin URI: http://www.ipgp.net
+Description: Show user's country flag.
+Author: Lucian Apostol
+Version: 0.3
+Author URI: http://www.ipgp.net
+*/
+
+
+function ipgp_flag() 
+{
+
+$ip =  getenv('REMOTE_ADDR'); 
+
+
+$xml = simplexml_load_file("http://www.ipgp.net/api/xml/". $ip ."wordpressplugin);
+
+// print_r($xml);
+
+if($xml->Flag && $xml->Code) { 
+  ?>
+  <a href="http://www.ipgp.net"><img src="<?=$xml->Flag?>" /></a>
+
+  <?
+	}
+ }
+
+function ipgpCountryWidget($args) {
+
+//	 extract($args);
+//	 echo $before_widget;
+	 ipgp_flag();
+//	 echo $after_widget;
+}
+
+function ip_country_shortcode( $atts ) {
+	
+	return ipgp_flag();
+
+}
+
+
+
+
+
+function ipgpInit()
+{
+  register_sidebar_widget(__('Ipgp Country Flag'), 'ipgpCountryWidget');  
+  add_shortcode( 'ipflag', 'ip_country_shortcode' );
+}
+
+
+add_action("plugins_loaded", "ipgpInit");
+?>
